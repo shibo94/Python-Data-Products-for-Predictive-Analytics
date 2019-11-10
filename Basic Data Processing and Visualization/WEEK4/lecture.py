f7704823ca88cf7c3f885ce769693c8f5f451a01
shibo94 <aiguozhedaodan@foxmail.com>
2019-11-10 19:52:48 +0100

    Update lecture.py

--- Basic Data Processing and Visualization/WEEK4/lecture.py
+++ Basic Data Processing and Visualization/WEEK4/lecture.py
@@ -20,3 +20,23 @@ funny = [d['funny'] for d in dataset]
 useful = [d['useful'] for d in dataset]
 
 ratings = numpy.array(ratings)
+cool = numpy.array(cool)
+funny = numpy.array(funny)
+useful = numpy.array(useful)
+ratings
+
+numpy.mean(ratings)
+numpy.var(ratings)
+numpy.stack([cool,funny,useful])
+features=numpy.stack([cool,funny,useful]).T
+features=numpy.matrix(features)
+numpy.linalg.inv(features.T * features)
+2*numpy.sin(features)+3
+2*numpy.sin(features)+3 > 4
+# ndarray.shape : get the shape of an array.
+# reshape: change the dimensions of an array or matrix. 
+# arange : create an array containing a range of numbers
+# Numpy.random : generates arrays of random numbers.
+# sum, min ,max, etc :reduction operations on matrices
+# Eye: identity matrices. 
+# trace, eig, etc: Linear algebra operations
