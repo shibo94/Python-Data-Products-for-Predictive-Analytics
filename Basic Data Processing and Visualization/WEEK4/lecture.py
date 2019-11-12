74b9685b8cdca81a95d0762b0f0f10b9e5644ceb
shibo94 <aiguozhedaodan@foxmail.com>
2019-11-12 18:41:36 +0100

    Update lecture.py

--- Basic Data Processing and Visualization/WEEK4/lecture.py
+++ Basic Data Processing and Visualization/WEEK4/lecture.py
@@ -40,3 +40,45 @@ numpy.linalg.inv(features.T * features)
 # sum, min ,max, etc :reduction operations on matrices
 # Eye: identity matrices. 
 # trace, eig, etc: Linear algebra operations
+
+#L2 Introduction to Data Visualization
+#L3 Introduction to Matplotlib
+import json
+import time
+path = "/review.json"
+f=open(path 'r')
+
+dataset = []
+for i in range(50000):
+    dataset.append(json.loads(f.readline()))
+datasetWithTimeValues = []
+for d in dataset:
+    d['data']
+    d['timeStruct'] = time.strptime(d['data'],"%Y-%m-%d")
+    d['timeInt'] = time.mktime(d['timeStruct'])
+    datasetWithTimeValues.append(d)
+
+from collections import defaultdict
+weekRatings = defaultdict(list)
+for d in datasetWithTimeValues:
+    day = d['timeStruct'].tm_wday
+    weekRatings[day].append(d['stars'])
+weekAverages = {}
+for d in weekRatings:
+    weekAverages[d]=sum(weekRatings[d]*1.0/len(weekRatings[d]))
+weekAverages
+x = list(weekAverages,keys())
+Y=[weekAverages[x] for x in X]
+import matplotlib.pylot as plt
+plt.plot(X,Y)
+plt.bar(X,Y)
+# zoom in more to see the detail
+plt.ylim(3.6, 3.8)
+plt.bar(X, Y)
+
+plt.ylim(3.6,3.8)
+plt.xlabel("Weekday")
+plt.ylabel("Rating")
+plt.xticks([0,1,2,3,4,5,6],['S','M','T','W','T','F','S'])
+plt.title("Rating as a function of weekday")
+plt.bar(X,Y)
