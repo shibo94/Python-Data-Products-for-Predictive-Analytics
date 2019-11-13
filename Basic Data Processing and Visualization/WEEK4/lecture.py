81cc135c7afec83ded052bd18bc0d43d973debaa
shibo94 <aiguozhedaodan@foxmail.com>
2019-11-13 11:04:10 +0100

    Update lecture.py

--- Basic Data Processing and Visualization/WEEK4/lecture.py
+++ Basic Data Processing and Visualization/WEEK4/lecture.py
@@ -82,3 +82,75 @@ plt.ylabel("Rating")
 plt.xticks([0,1,2,3,4,5,6],['S','M','T','W','T','F','S'])
 plt.title("Rating as a function of weekday")
 plt.bar(X,Y)
+
+#L4 Live-coding: MatPlotLib
+path = "datasets/yelp_data/review.json"
+f = open(path,'r',encoding = 'utf8')
+import json
+import time
+dataset = []
+for i in range(50000):
+    d = json.loads(f.readline())
+    # d['data']
+    d['timeStruct'] = time.strptime(d['data'],'%Y-%m-%d')
+    d['timeInt'] = time.mktime(d['timeStruct'])
+    dataset.append(d)
+dataset[0]
+from collections import defaultdict
+weekRatings = defaultdict(list)
+for d in dataset:
+    day = d['timeStruct'].tm_wday
+    weekRatings[day].append(d['stars'])
+weekAverages = {}
+for d in weekRatings:
+    weekAverages[d] = sum(weekRatings[d])/len(weekRatings[d])
+weekAverages
+from matplotlib import pyplot as plt
+X = list(weekAverages,keys())
+X
+Y= [weekAverages[x] for x in X]
+plt.plot(X,Y)
+plt.show() 
+plt.bar(X,Y)
+plt.show()
+plt.ylim(3.6, 3.8)
+plt,bar(X,Y)
+plt.show()
+plt.xlabel("Weekday")
+plt.ylabel("Rating")
+plt.title("Rating as a function of weekday")
+plt.bar(X,Y)
+plt.show()
+
+plt.ylim(3,6, 3.8)
+plt.xlabel("Weekday")
+plt.ylabel("Rating")
+plt.xticks([0,1,2,3,4,5,6],['S','M','T','W','T','F','S'])
+plt.title("xxx")
+plt.bar(X,Y)
+#L5 urllib and BeautifulSoup
+from urllib.request import urlopen
+f = urlopen("https://www.goodreads.com/book/show/4671.The_Great_Gatsby")
+html = str(f.read())
+reviews = html.split('<div id="review_')[1:]
+len(reviews)
+review[0]
+
+def parseReview(review):
+    d={}
+    d['stars'] = review.split('<span class=" staticStars notranslate" title="')[1].split['"'][0]
+    d['date'] = review.split('<a class="reviewDate')[1].split('>')[1].split('<')[0]
+    d['user'] = review.split('<a title="')[1].split['"'][0]
+    shelves = []
+    try:
+        shelfBlock = review.split('<div class="uitext greyText bookshelves">')[1].split('</div>')[0]
+        for s in shelfBlock.split('shelf=')[1:]:
+            shelves.append(s.split('"')[0])
+        d['shelves'] = shelves
+    except Exception as e:
+        pass
+    reviewBlock = rebiew.split('<div class="reviewText stacked">')[1].split('</div')[0]
+    d['reviewBlock'] = reviewBlock
+    return d
+reviewDict = [parseReview(r) for r in reviews]
+reviewDict[0]
