c8bced7a4100ca287185961b2f7015d6fcfaa33c
shibo94 <aiguozhedaodan@foxmail.com>
2019-11-20 16:07:44 +0100

    Update lecture.py

--- Design Thinking and Predictive Analytics for Data Products/WEEK1/lecture.py
+++ Design Thinking and Predictive Analytics for Data Products/WEEK1/lecture.py
@@ -25,3 +25,21 @@ def feature(datum):
 X = [feature(d) for d in dataset]
 y[:10]
 X[:10]
+theta,residuals,rank,s = numpy.linalg.lstsq(X,y)
+theta
+[out] pm2.5 = 107.1 - 0.68*temp
+
+def feature(datum):
+    feat = [1, float(datum[7]), float(datum[8]),float(datum[10])]
+    return feat
+X = [feature(d) for d in dataset]
+theta, residuals, rank, s = numpy.linalg.lstsq(X,y)
+theta
+[out] array([3.26e+03, aaa, bbb, ccc, ddd])
+X=numpy.matrix(X)
+y=numpy.matrix(y)
+numpy.linalg.inv(X.T * X) * X.T *y.T
+matrix([3.26],
+        [aaa],
+        [bbb],
+        [ccc])
