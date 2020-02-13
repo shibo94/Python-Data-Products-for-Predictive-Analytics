baf016b213f0138ed50a3cd560330be48d5364a0
shibocs <aiguozhedaodan@foxmail.com>
2020-02-13 22:09:36 +0100

    Update default.py

--- Design Thinking and Predictive Analytics for Data Products/WEEK4/default.py
+++ Design Thinking and Predictive Analytics for Data Products/WEEK4/default.py
@@ -104,3 +104,105 @@ dataset = [d for d in dataset if d[5] != 'NA']
 def feature(datum):
     feat = [1, float(datum[7])]
     return feat
+X = [feature(d) for d in dataset]
+y = [float(d[5]) for d in dataset]
+X[0]
+[1, -4.0]
+K = len(X[0])
+K
+theta = [0.0]*K
+theta[0] = sum(y) / len(y)
+def inner(x,y):
+    return sum([a*b for (a,b) in zip(x,y)])
+def norm(x):
+    return sum([a*a for a in x])
+
+def derivative(X,y,theta):
+    dtheta = [0.0]*len(theta)
+    K = len(theta)
+    N = len(X)
+    MSE = 0
+    for i in range(N):
+        error = inner(X[i],theta)-y[i]
+        for k in range(k):
+            dtheta[k] += 2*X[i][k]*error/N
+            MSE += error*error/N
+        return dtheta, MSE
+MSE： MEAN SQUARED ERROR
+
+learningRate = 0.003
+while (True):
+    dtheta.MSE = derivative(X,y,theta)
+    m = norm(dtheta)
+    print("norm(dtheta) = " + str(m) + " MSE = " + str(MSE))
+    for k in range(K):
+        theta[k] -= learningRate * dtheta[k]
+        if m < 0.01 : break
+
+#L4 Gradient Descent in TensorFlow
+import tensorflow as tf
+path = "datasets/PRSA_data_2010.1.1-2014.12.31.csv"
+f = open(path,'r')
+dataset = []
+header = f.readline().strip().split(',')
+for line in f:
+    line = line.split(',')
+    dataset.append(line)
+
+header.index('pm2.5')
+dataset = [d for d in dataset if d[5] != 'NA']
+def feature(datum):
+    feat = [1, float(datum[7]), float(datum[8]), float(datum[10])] #Temperature, pressure, and wind speed
+    return feat
+X = [feature(d) for d in dataset]
+y = [float(d[5]) for d in dataset]
+y = tf.constant(y, shape=[len(y), 1])
+K =len(X[0])
+
+def MSE(X,y,theta):
+    return tf.reduce_mean((tf.matmul(X,theta) - y) ** 2)
+
+theta = tf.Variable(tf.constant([0,0]*K, shape=[K,1]))
+
+Initialized to zero
+
+optimizer = tf.train.AdamOptimizer(0.01)
+
+Stochastic gradient descent opimizer with learning rate of 0.01
+
+objective = MSE(X, y, theta)
+
+train = optimizer.minimize(objective)
+
+We want to minimize the objective
+
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+for iteration in range(1000):
+    cvalues = sess.run([train, objective])
+    print("objective = " + str(cvalues[1]))
+# So, theta might have an initial value like might be a vector of zeros, but I'm also telling Tensorflow that vector is a vector of variables that I would like TensorFlow to optimize for me. So here, I'm just initializing theta to be a vector of zeros of length k, and I'm telling you that this a variable. Next is a few more boilerplate Tensorflow polarizations. I'm telling it what optimizer I'd like to use, it is what's called a Stochastic gradient descent optimizer with learning rate of 0.01 and finally, I'm telling it what is the objective I want to optimize. Okay. So I'm telling it what I would like to do is optimize the mean squared error and theta is going to be the variable that can be modified in order to minimize this. So, it's important to note that everything I've done so far is really just specifying the problem. No computation is actually performed yet.
+# Start transcript at 4 minutes 0 seconds4:00
+# Okay. Next is a little bit more boilerplate for initializing the optimizer. The first thing I'd do is just tell it this is an objective that should be minimized and I use this Tensorflow function to perform the initialization,
+    
+with sess.as_default():
+    print(MSE(X, y, theta).eval())
+    print(theta.eval())
+    
+Livecoding: Tensorflow
+path = "datasets/PRSA_data_2010.1.1-2014.12.31.csv"
+f = open(path, 'r')
+
+dataset = []
+header = f.readline().strip().split(',')
+for line in f:
+    line = line.split(',')
+    dataset.append(line)
+
+header.index('pm2.5')
+dataset = [d for d in dataset if d[5] != 'NA']
+def feature(datum):
+    feat = [1, float(datum[7])]
+    return feat
