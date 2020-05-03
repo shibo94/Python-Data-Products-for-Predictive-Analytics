1fb423d6164fe4f5451fb45471ce7a63c6d3cb6d
shibocs <aiguozhedaodan@foxmail.com>
2020-05-03 12:37:23 +0200

    Update default.py

--- Deploying Machine Learning Models/WEEK1/default.py
+++ Deploying Machine Learning Models/WEEK1/default.py
@@ -17,3 +17,53 @@ f = gzip.open(path,"rt",encoding="utf8")
 
 header = f.readline()
 header = header.strip().split('\t')
+dataset=[]
+for line in f:
+    fields = line.strip().split('\t')
+    d = dict(zip(header,fields))
+    d['star_rating'] = int(d['star_rating'])
+    d['helpful_votes'] = int(d['helpful_votes'])
+    d['total_votes'] = int(d['total_votes'])
+    dataset.append(d)
+
+usersPerItem = defaultdict(set)
+itemsPerUser = defaultdict(set)
+itemNames={}
+for d in dataset:
+    user,item = d['customer_id'],d['product_id']
+    usersPerItem[item].add(user)
+    itemsPerUser[user].add(item)
+    itemNames[item] = d['product_title']
+
+def Jaccard(s1,s2):
+    numer = len(s1,intersection(s2))
+    denom = len(s1.union(s2))
+    return numer / denom
+
+def mostSimilar(i):
+    similarities=[]
+    users = usersPerItem[i]
+    for i2 in usersPerItem:
+        if i2 == i: continue
+        sim = Jaccard(users,usersPerItem[i2])
+        similarities.append((sim,i2))
+    similarities.sort(reverse=True)
+    return similarities[:10]
+dataset[2]
+query = dataset[2]['product_id']
+mostSimilar(query)
+itemNames[query]
+[itemNames[x[1]] for x in mostSimilar(query)]
+
+def mostSimilarFast(i):
+    similarities=[]
+    users = usersPerItem[i]
+    candidateItems = set()
+    for u in users:
+        candidateItems = candidateItems.union(itemsPerUser[u])
+    for i2 in candidateItems:
+        if i2 == i: continue
+        sim = Jaccard(users,usersPerItem[i2])
+        similarities.append((sim,i2))
+    similarities.sort(reverse=True)
+    return similarities[:10]
